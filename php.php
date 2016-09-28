4b9c982c66b46da45a72e2ce400d37c2d3dacf1b
André Isaque <andre.fc_isaque@outlook.pt>
2016-09-29 00:12:51 +0100

    Simple Contact Form
    This project involves simple HTML and PHP and consists of a basic
    contact form that basically asks the user to fill in the data, and will
    be returned by the function in PHP the data entered.

---
+++ php.php
@@ -0,0 +1,25 @@
+﻿<?php
+	
+	$nome = $_POST["nome"];
+	$morada = $_POST["morada"];
+	$email = $_POST["email"];
+	$genero = $_POST["genero"];
+	$hab = $_POST['hab'];
+	$ling = array();
+	if( isset($_POST["ling"])){
+		$ling = $_POST["ling"];
+	}
+	$obs = $_POST['obs'];
+
+	echo 'Nome: ' .$nome.'<br>';
+	echo 'Morada: ' .$morada.'<br>';
+	echo 'Email: ' .$email.'<br>';
+	echo 'Género: ' .$genero.'<br>';
+	echo 'Habilitações: ' .$hab.'<br>';
+	echo 'Linguagens:';
+		for($i = 0;$i < count($ling); $i++){
+			echo $ling[$i]."";
+		}
+	echo '<br>Observações: ' .$obs.'<br>';
+
+?>
